aeeb41fc9b0ff06731cb98097a7dfb31f33fe98f
gitanjali-13 <gitaanjalide@gmail.com>
2022-09-08 14:57:38 +0530

    Refactoring area and perimeter of triangle

--- src/com/bridgelabz/practice/AreaAndPerimeter.java
+++ src/com/bridgelabz/practice/AreaAndPerimeter.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class AreaAndPerimeter {
-    double r,area ,length,width,perimeter;
+    double r,area,length,width,side1,side2,side3;
+    static double perimeter;
     Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -12,47 +13,23 @@ public class AreaAndPerimeter {
         AreaAndPerimeter a1 = new AreaAndPerimeter();
         System.out.println("--Please select your option--");
         System.out.println("MENU");
-        System.out.println("1.Area of triangle:");
-        System.out.println("2.Perimeter of triangle:");
-        System.out.println("3.Area of circle:");
-        System.out.println("4.Perimeter of circle:");
-        System.out.println("5.Area of rectangle:");
-        System.out.println("6.Perimeter of rectangle:");
+        System.out.println("1.Triangle:");
+        System.out.println("2.Circle");
+        System.out.println("3.Rectangle");
+        System.out.println("4.");
 
         int num = a1.sc.nextInt();
         switch (num)
         {
             case 1 :
-                //Scanner sc = new Scanner(System.in);
-                System.out.println("Enter length of the triangle:");
-                a1.length = a1.sc.nextDouble();
-                System.out.println("Enter width of the triangle:");
-                a1.width = a1.sc.nextDouble();
-                a1.area = (a1.length * a1.width )/2;
-                System.out.println("Area of the triangle" +a1.area);
+                a1.triangle();
                 break;
+
             case 2 :
-                double side1,side2,side3,perimeter;
-                System.out.println("Enter first side of  triangle:");
-                side1 = a1.sc.nextDouble();
-                System.out.println("Enter second side of  triangle");
-                side2 = a1.sc.nextDouble();
-                System.out.println("Enter third side of triangle");
-                side3 = a1.sc.nextDouble();
-                perimeter = side1 + side2 + side3;
-                System.out.println("Area of the triangle" +perimeter);
-                break;
-            case 3 :
                 a1.circle();
                 break;
-            case 4 :
-                //double pi = 3.14;
-                System.out.println("Enter redius of the circle:");
-                a1.r = a1.sc.nextDouble();
-                perimeter = 2 * Math.PI * a1.r;
-                System.out.println("Perimeter of the circle:" +perimeter);
-                break;
-            case 5 :
+
+            case 3 :
                 System.out.println("Enter length of the rectangle:");
                 a1.length = a1.sc.nextDouble();
                 System.out.println("Enter width of the rectangle");
@@ -60,7 +37,7 @@ public class AreaAndPerimeter {
                 a1.area = a1.length * a1.width;
                 System.out.println("Area of the rectangle :" +a1.area);
                 break;
-            case 6 :
+            case 4 :
 
                 System.out.println("Enter length of the rectangle:");
                 a1.length = a1.sc.nextDouble();
@@ -79,9 +56,22 @@ public class AreaAndPerimeter {
         System.out.println("Area of the circle:" +area);
         perimeter = 2 * Math.PI * r;
         System.out.println("Perimeter of the circle:" +perimeter);
-
     }
-
-
+    public  void  triangle(){
+        System.out.println("Enter first side of the triangle:");
+        side1 = sc.nextFloat();
+        System.out.println("Enter second side of the triangle");
+        side2 = sc.nextFloat();
+        System.out.println("Enter third side of the triangle");
+        side3 = sc.nextFloat();
+        perimeter = side1 + side2 + side3;
+        System.out.println("Perimeter of the triangle" +perimeter);
+        System.out.println("Enter length of the triangle:");
+        length = sc.nextFloat();
+        System.out.println("Enter width of the triangle:");
+        width = sc.nextFloat();
+        area = (length * width )/2;
+        System.out.println("Area of the triangle" +area);
+    }
 
 }
